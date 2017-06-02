3c92fb1932c8143eb9471de5a33d2abb2dd6fc10
Ngoc Tuan <tuansaker1412@gmail.com>
2017-06-02 15:42:08 +0700

    Finish chap7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: :show
+
   def new
+   @user = User.new
+  end
+
+  def show;
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = t ".welcome"
+      redirect_to @user
+    else
+      flash.now[:danger] = t ".fail"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+
+    return if @user
+      render file: "public/404.html", layout: false
   end
 end
